020e24ff252e4a6ec2a703d962d57c92013ee430
Miguel Angel Baquero <migue_uio93@hotmail.com>
2026-04-21 01:26:29 -0500

    feat (app): Agregar politicas de CORS
    Se agrego middleware para CORS

---
+++ middlewares/cors.js
@@ -0,0 +1,20 @@
+import cors from 'cors';
+
+const ACCEPTED_ORIGINS = [
+  `http://localhost:${process.env.PORT}`,
+  `https://localhost:${process.env.PORT}`,
+  `https://document-generator-pi.vercel.app`,
+  `https://document-generator-60lxaxdwo-miguel-baqueros-projects.vercel.app/`,
+];
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
+  return cors({
+    origin: (origin, callback) => {
+      // console.debug('Origin:', origin); // Log the origin
+      if (acceptedOrigins.includes(origin) || !origin) {
+        return callback(null, true);
+      }
+      return callback(new Error('Not allowed by CORS'));
+    },
+  });
+};
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import PizZip from 'pizzip';
 import Docxtemplater from 'docxtemplater';
+import { corsMiddleware } from './middlewares/cors.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 const NODE_ENV = process.env.NODE_ENV ?? 'development';
 
-app.use(cors());
+app.use(corsMiddleware());
 app.use(express.json());
 app.use(express.static(__dirname));
 
