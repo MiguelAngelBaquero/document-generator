5481550fac6fe1270ad02e4580e8438a4ab016d2
Miguel Angel Baquero <migue_uio93@hotmail.com>
2026-04-29 22:37:59 -0500

    feat (vercel): Fix CORS for vercel production environment
    Modificacion de cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 const ACCEPTED_ORIGINS = [
   `http://localhost:${process.env.PORT}`,
   `https://localhost:${process.env.PORT}`,
-  `https://document-generator-pi.vercel.app`,
+  `https://document-generator-h04wajp2e-miguel-baqueros-projects.vercel.app`,
+  `https://project-hd3cj.vercel.app`,
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
@@ -13,7 +14,7 @@ export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
       if (acceptedOrigins.includes(origin) || !origin) {
         return callback(null, true);
       }
-      
+
       // Permitir dinámicamente todas las URLs generadas por Vercel (Previews y Producción)
       if (origin.endsWith('.vercel.app')) {
         return callback(null, true);
